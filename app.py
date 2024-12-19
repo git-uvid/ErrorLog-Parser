66256517b5096c2143c48c5aeec19e6f0a786d9c
git-uvid <git@uvidconsulting.com>
2024-12-19 12:42:17 +0530

    Update app.py
    Added New pattern of log .

--- app.py
+++ app.py
@@ -11,8 +11,9 @@ st.set_page_config(
 def parse_log_file(file):
     date_pattern = r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3}'
     warn_pattern = r'(?:WARN :)'
-    pattern = r"Coordinate (\S+) not found in dimension (\S+) \(load (\S+)\)"
-
+    pattern_1 = r"Coordinate (\S+) not found in dimension (\S+) \(load (\S+)\)"
+    pattern_2 = r"Could not write cube cell: Element (\S+) in dimension (\S+) is consolidated and Splashing is disabled. \(load (\S+)\)"
+    
     data = []
     other_warnings = []
 
@@ -21,18 +22,32 @@ def parse_log_file(file):
     for line in file_content.splitlines():
         processed_date = re.sub(date_pattern, '', line)
         processedWarn_date = re.sub(warn_pattern, '', processed_date)
-        match = re.search(pattern, processedWarn_date)
-        if match:
-            coordinate = match.group(1)
-            dimension = match.group(2)
-            load = match.group(3)
+        
+        # Check if the line matches pattern_1
+        match_1 = re.search(pattern_1, processedWarn_date)
+        if match_1:
+            coordinate = match_1.group(1)
+            dimension = match_1.group(2)
+            load = match_1.group(3)
             data.append([coordinate, dimension, load])
+        
+        # Check if the line matches pattern_2
         else:
-            line_without_date_warn = re.sub(r"^\S+ \S+, \S+  WARN :", "", processedWarn_date)
-            other_warnings.append([line_without_date_warn])
+            match_2 = re.search(pattern_2, processedWarn_date)
+            if match_2:
+                element = match_2.group(1)
+                dimension = match_2.group(2)
+                load = match_2.group(3)
+                data.append([element, dimension, load])
+            else:
+                line_without_date_warn = re.sub(r"^\S+ \S+, \S+  WARN :", "", processedWarn_date)
+                other_warnings.append([line_without_date_warn])
+
     return data, other_warnings
 
+
 st.title("ErrorLog Parser")
+st.write("This tool simplifies troubleshooting, enhances debugging efficiency, and allows users to quickly generate downloadable Excel reports, making it easier to track, manage, and resolve integration process-related problems.")
 
 uploaded_file = st.file_uploader("Choose a log file", type="txt")
 if uploaded_file is not None:
